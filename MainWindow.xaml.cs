153b5bec283c818948e5bd98f1f7cc34fa01ce57
Kateryna <kostucenkokata0@gmail.com>
2023-11-29 20:02:26 +0200

    0

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,11 @@
-﻿using System;
+﻿using ClassLibrary1;
+using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using static ClassLibrary1.Class1;
 
 namespace ADOWPF1
 {
@@ -20,9 +14,89 @@ namespace ADOWPF1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ViewModel viewModel;
+        const string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=storage;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+        Class1 databaseManager;
         public MainWindow()
         {
             InitializeComponent();
+            databaseManager = new Class1(connectionString);
+            viewModel = new ViewModel();
+            DataContext = viewModel;
+        }
+        private void Border_Click(object sender, MouseButtonEventArgs e)
+        {
+            ConnectToDatabase();
+        }
+            private void Border_Click2(object sender, MouseButtonEventArgs e)
+            {
+            DG_Users.ItemsSource = viewModel.GetAllProducts();
+        }
+        private void Border_Click3(object sender, MouseButtonEventArgs e)
+        {
+            DG_Users.ItemsSource = viewModel.GetAllProducts();
+        }
+
+        private void ConnectToDatabase()
+        {
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    MessageBox.Show("Підключено до бази даних.");
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Помилка підключення до бази даних: " + ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                    MessageBox.Show("Відключено від бази даних.");
+                }
+            }
+
+        }
+        private void ShowAllInformation_Click(object sender, RoutedEventArgs e)
+        {
+            DG_Users.ItemsSource = viewModel.GetAllProducts();
+        }
+        private void ShowAllProducts_Click(object sender, RoutedEventArgs e)
+        {
+            DG_Users.ItemsSource = viewModel.Products;
+        }
+        private void ShowProductsByCategory_Click(object sender, RoutedEventArgs e)
+        {
+            string selectedCategory = "Тип1"; 
+            DG_Users.ItemsSource = viewModel.GetProductsByCategory(selectedCategory);
+        }
+
+        private void ShowProductsBySupplier_Click(object sender, RoutedEventArgs e)
+        {
+            string selectedSupplier = "Постачальник1"; 
+            DG_Users.ItemsSource = viewModel.GetProductsBySupplier(selectedSupplier);
+        }
+
+        private void ShowOldestProductInStock_Click(object sender, RoutedEventArgs e)
+        {
+            DG_Users.ItemsSource = new List<Product> { viewModel.GetOldestProductInStock() };
+        }
+
+        private void ShowAverageQuantityByProductType_Click(object sender, RoutedEventArgs e)
+        {
+            var averageQuantities = viewModel.GetAverageQuantityByProductType();
+
+            var averageQuantityProducts = averageQuantities.Select(kv => new Product
+            {
+                ProductType = kv.Key,
+                Quantity = (int)kv.Value 
+            }).ToList();
+
+            DG_Users.ItemsSource = averageQuantityProducts;
         }
     }
 }
+
---
+++ ViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using static ClassLibrary1.Class1;
+
+public class ViewModel
+{
+    public ObservableCollection<Product> Products { get; set; }
+    public IEnumerable ProductTypes { get; internal set; }
+    public IEnumerable Suppliers { get; internal set; }
+
+    public ViewModel()
+    {
+        Products = new ObservableCollection<Product>
+        {
+            new Product { ProductName = "Продукт1", ProductType = "Тип1", Supplier = "Постачальник1", Quantity = 10, CostPrice = 100, SupplyDate = DateTime.Now },
+            new Product { ProductName = "Продукт2", ProductType = "Тип2", Supplier = "Постачальник2", Quantity = 20, CostPrice = 200, SupplyDate = DateTime.Now.AddDays(-5) },
+           
+        };
+    }
+
+    public List<Product> GetAllProducts()
+    {
+        return Products.ToList();
+    }
+
+    public List<string> GetAllProductTypes()
+    {
+        return Products.Select(p => p.ProductType).Distinct().ToList();
+    }
+
+    public List<string> GetAllSuppliers()
+    {
+        return Products.Select(p => p.Supplier).Distinct().ToList();
+    }
+
+    public Product GetProductWithMaxQuantity()
+    {
+        return Products.OrderByDescending(p => p.Quantity).FirstOrDefault();
+    }
+
+    public Product GetProductWithMinQuantity()
+    {
+        return Products.OrderBy(p => p.Quantity).FirstOrDefault();
+    }
+
+    public Product GetProductWithMinCostPrice()
+    {
+        return Products.OrderBy(p => p.CostPrice).FirstOrDefault();
+    }
+
+    public Product GetProductWithMaxCostPrice()
+    {
+        return Products.OrderByDescending(p => p.CostPrice).FirstOrDefault();
+    }
+    public List<Product> GetProductsByCategory(string category)
+    {
+        return Products.Where(p => p.ProductType == category).ToList();
+    }
+
+    public List<Product> GetProductsBySupplier(string supplier)
+    {
+        return Products.Where(p => p.Supplier == supplier).ToList();
+    }
+
+    public Product GetOldestProductInStock()
+    {
+        return Products.OrderBy(p => p.SupplyDate).FirstOrDefault();
+    }
+
+    public Dictionary<string, double> GetAverageQuantityByProductType()
+    {
+        var averageQuantityByType = Products
+            .GroupBy(p => p.ProductType)
+            .ToDictionary(g => g.Key, g => g.Average(p => p.Quantity));
+
+        return averageQuantityByType;
+    }
+}
